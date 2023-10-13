110453ddbe753484817e9081ac0c92e1fb837b56
Bohan Li <liboha@amazon.com>
2023-10-13 11:18:40 -0700

    Updated Catalog API CLI examples to use the newly released DetailsDocument field

--- catalog-api/products/ami/restrict-delivery-options/example.sh
+++ catalog-api/products/ami/restrict-delivery-options/example.sh
@@ -6,8 +6,8 @@ VERSION_TITLE_TO_RESTRICT="new-version"
 # Extract DeliveryOptionId for the version to restrict
 DELIVERY_OPT_ID=$(aws marketplace-catalog describe-entity \
   --catalog AWSMarketplace \
-  --entity-id 09c969cd-8db3-4c87-9181-a9d45602267d \
-  --query 'Details' --output text |
+  --entity-id 10000000-0000-0000-0000-000000000000 \
+  --query 'DetailsDocument' |
   jq '.Versions[] | select(.VersionTitle == "'"${VERSION_TITLE_TO_RESTRICT}"'") | .DeliveryOptions[].Id')
 
 if [ -z "$DELIVERY_OPT_ID" ]; then
@@ -16,12 +16,12 @@ if [ -z "$DELIVERY_OPT_ID" ]; then
 fi
 
 # Set details JSON
-DETAILS_JSON_AS_STRING=$(echo '
+DETAILS_JSON=$(echo '
 {
   "DeliveryOptionIds": [
     '${DELIVERY_OPT_ID}'
   ]
-}' | jq "tostring")
+}')
 
 # Run AWS CLI command
 # Entity identifier is same as the productId
@@ -34,6 +34,6 @@ aws marketplace-catalog start-change-set \
           "Identifier": "10000000-0000-0000-0000-000000000000",
           "Type": "AmiProduct@1.0"
         },
-        "Details": '"${DETAILS_JSON_AS_STRING}"'
+        "DetailsDocument": '${DETAILS_JSON}'
       }
     ]'
